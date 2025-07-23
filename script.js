5a1f9ee43efb18939aa66341d770d1cd8b5c5ed3
SoloGudLyf <solomonteiko0@gmail.com>
2025-07-23 05:53:30 +0000

    feat(HTML, CSS, JS): Write comments to guide                      To guide in the implementation of the program.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Start");
+// Initialize a board array whose values will be 0
+
+//Initialize 2 player variables
+
+// Assign the divs to the board array
+
+//Check if a div has already been clicked if so then maintain the value it corresponds with in
+// the object and DOM
+
+// Set up a click event handler for all the divs and change their value to 1
+// when clicked on the first time and 2 when clicked on the second time toggling between these
+// two modes
+
+//set up a conditional statement to get when a player wins or not or draw
+
+// Display a message on the page as to which player has won and lost
